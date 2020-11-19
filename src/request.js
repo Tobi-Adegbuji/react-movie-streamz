a12f2c6ff3d3bf226a61332674dff41fb93d1a9e
Tobi <adegbuji_tobi@yahoo.com>
2020-11-18 19:46:42 -0500

    axios added

--- src/request.js
+++ src/request.js
@@ -1,7 +1,7 @@
 const API_KEY = "32a6b11ea9b8dd60035deb691b3768b"; 
 
 const endpoints = {
-    const: "https://api.themoviedb.org/3/trending/all/day?api_key=" + API_KEY
+    trendingMovies: "/trending/all/day?api_key=${API_KEY}&language=en-US"
 }
 
 export default endpoints; 
